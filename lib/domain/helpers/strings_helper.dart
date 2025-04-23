a1803899e3d77a9a61db76ce71b3b775af1f228b
benFactotum99 <74727114+benFactotum99@users.noreply.github.com>
2025-04-23 21:42:02 +0200

    correzioni

--- lib/domain/helpers/strings_helper.dart
+++ lib/domain/helpers/strings_helper.dart
@@ -1,5 +1,6 @@
+//String baseUrl = "http://5.196.7.11:3001/Api";
 String baseUrl = "http://localhost:3001/Api";
 
 String CURRENT_USER = "C3j9g9RURQHwORusUKZ0lbfVFxx2O1yA65cYO1x56j";
 
-double RANKING = 0.80;
+double RANKING = 0.35;
